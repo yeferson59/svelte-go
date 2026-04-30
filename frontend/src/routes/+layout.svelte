eed717e39ed97441fa3ecd4eb473dbb82014ecb1
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-04-29 19:02:59 -0500

    feat: add Vercel Analytics and inject in layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
