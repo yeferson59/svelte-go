657b0b5cafc51510d7008240bed860d689dd2881
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-02 18:24:38 -0500

    fix: add validation request login dto

--- backend/internal/dtos/auth/request.go
+++ backend/internal/dtos/auth/request.go
@@ -8,5 +8,5 @@ type RegisterRequestDTO struct {
 
 type LoginRequestDTO struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
+	Password string `json:"password" validate:"required,min=8,max=20"`
 }
