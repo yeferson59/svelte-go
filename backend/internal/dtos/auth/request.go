4f5dc613053db6ea1cb903779f3751e0eb86ec04
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-02 18:26:42 -0500

    feat: more validation for request register dto

--- backend/internal/dtos/auth/request.go
+++ backend/internal/dtos/auth/request.go
@@ -1,8 +1,8 @@
 package auth
 
 type RegisterRequestDTO struct {
-	Name     string `json:"name" validate:"required,min=2"`
-	Email    string `json:"email" validate:"required,email"`
+	Name     string `json:"name" validate:"required,min=2,max=254"`
+	Email    string `json:"email" validate:"required,email,min=2,max=254"`
 	Password string `json:"password" validate:"required,min=8,max=20"`
 }
 
