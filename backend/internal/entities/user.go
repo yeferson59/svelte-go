fb8d9e12fba226e2bb2877c37650061a904b6ff3
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-02 19:22:38 -0500

    feat: add roles for authentication and authorization

--- backend/internal/entities/user.go
+++ backend/internal/entities/user.go
@@ -6,12 +6,22 @@ import (
 	"github.com/google/uuid"
 )
 
+type Role struct {
+	ID          uuid.UUID  `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
+	DeletedAt   *time.Time `json:"deletedAt"`
+}
+
 type User struct {
 	ID            uuid.UUID  `json:"id"`
 	Name          string     `json:"name"`
 	Email         string     `json:"email"`
 	EmailVerified bool       `json:"emailVerified"`
 	Image         string     `json:"image"`
+	RoleID        uuid.UUID  `json:"roleId"`
 	CreatedAt     time.Time  `json:"createdAt"`
 	UpdatedAt     time.Time  `json:"updatedAt"`
 	DeletedAt     *time.Time `json:"deletedAt"`
